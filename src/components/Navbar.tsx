9b23ce9615bb20d6e9d405c02294fb0b1fff7c2d
Robbie Corcoran <codewithrobbie@gmail.com>
2024-03-04 18:44:06 +0100

    Made nav fixed to page

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -16,7 +16,7 @@ const Navbar = () => {
   ];
 
   return (
-    <nav className="bg-theme-primary text-theme-text">
+    <nav className="bg-theme-primary text-theme-text w-full fixed">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <div className="flex items-center space-x-3">
           <span className="self-center text-2xl font-semibold whitespace-nowrap">
@@ -26,7 +26,7 @@ const Navbar = () => {
         <button
           data-collapse-toggle="navbar-default"
           type="button"
-          className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
+          className="inline-flex items-center p-2 w-10 h-10 md:hidden"
           aria-controls="navbar-default"
           aria-expanded="false"
           onClick={toggleNav}
@@ -50,16 +50,16 @@ const Navbar = () => {
         </button>
         <div
           className={`w-full md:block md:w-auto ${
-            navOpen ? "block" : "hidden"
+            navOpen ? "block " : "hidden"
           }`}
           id="navbar-default"
         >
-          <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white">
+          <ul className="font-medium flex flex-col p-4 md:p-0 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white">
             {navItems.map((item) => (
               <li key={item.id}>
                 <a
                   href="#"
-                  className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0"
+                  className="block md:py-2 md:px-3 text-white md:p-0 text-right"
                   aria-current="page"
                 >
                   {item.text}
