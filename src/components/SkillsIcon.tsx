53109c18f9615fbebde24334eba25d5beee69f29
Robbie Corcoran <codewithrobbie@gmail.com>
2024-03-06 13:13:35 +0100

    Skills dymanically displaying icons in FE tab

---
+++ src/components/SkillsIcon.tsx
@@ -0,0 +1,20 @@
+import { SVGProps } from "react";
+
+interface SkillsIconProps extends SVGProps<SVGSVGElement> {
+  fileName: string;
+  folderName: string;
+}
+
+const SkillsIcon: React.FC<SkillsIconProps> = ({ fileName, folderName }) => {
+  return (
+    <>
+      <img
+        src={`../../public/skillIcons/${folderName}/${fileName}.svg`}
+        alt={`${fileName}`}
+        className="h-10 w-10"
+      />
+    </>
+  );
+};
+
+export default SkillsIcon;
