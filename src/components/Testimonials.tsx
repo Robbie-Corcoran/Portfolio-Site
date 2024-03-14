02de80a8b240a5518cf5f3aceefce2a42baf9110
Robbie Corcoran <codewithrobbie@gmail.com>
2024-03-14 12:03:55 +0100

    Added first testimonial

--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -12,11 +12,16 @@ const Testimonials = () => {
         </h2>
       </div>
       <TestimonialCard
-        authorImg=""
-        authorDescription=""
-        date=""
-        relationship=""
-        body=""
+        authorImg="testimonials/laylaSilbernberg/layla-portrait.jpeg"
+        authorName="Layla Silbernberg"
+        authorDescription="Master of Arts | Java Certified Professional | Full-stack Java Developer | Spring Framework | React | Next.js | Typescript | MongoDB | PostgreSQL"
+        date="1st Feb, 2024"
+        relationship="Java Developer & Instructor, Salt, NL"
+        body={[
+          `"Robbie is a funny and tenacious individual with a great eye for creative solutions, great design sense and an unmatched enthusiasm for tech.`,
+          `Furthermore, the gift of gab is strong with this one. He can both understand and communicatbe complex programming concepts and explain them well. And explain them funny.`,
+          `Truly a joy to work with, I believe Robbie will be asset to any team."`,
+        ]}
       />
     </section>
   );
