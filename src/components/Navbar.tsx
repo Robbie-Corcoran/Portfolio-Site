c2a7535ff41d16b539f2e4e3555e04b07b24f558
Robbie Corcoran <codewithrobbie@gmail.com>
2024-03-19 12:18:23 +0100

    Nav items padding changes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -48,7 +48,7 @@ const Navbar = () => {
             }`}
             id="navbar-default"
           >
-            <ul className="font-medium flex flex-col md:p-0 md:flex-row md:space-x-2 md:mt-0 md:border-0 md:bg-white gap-2">
+            <ul className="font-medium flex flex-col pt-2 pr-3 md:p-0 md:flex-row md:space-x-2 md:mt-0 md:border-0 md:bg-white gap-2">
               {navItems.map((item) => (
                 <li key={item.id}>
                   <a
