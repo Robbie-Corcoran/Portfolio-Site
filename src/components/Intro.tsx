769ad518883c8a13cff2cac1c47da82828e880bb
Robbie Corcoran <codewithrobbie@gmail.com>
2024-03-20 16:27:47 +0100

    Landing update

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -10,7 +10,7 @@ const Intro = () => {
             <br />
             <span className="text-8xl text-theme-primary font-bold hover:underline hover:decoration-theme-text hover:decoration-4">
               <ReactTyped
-                strings={["Robbie."]}
+                strings={["ROBBIE."]}
                 typeSpeed={120}
                 startDelay={800}
                 cursorChar="|"
