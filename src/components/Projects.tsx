5e68c16afe75b54a4d094e54cebb46fbf5238c2f
Robbie Corcoran <codewithrobbie@gmail.com>
2024-03-26 10:47:41 +0100

    Spelling and grammar fixes

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -13,21 +13,21 @@ const Projects = () => {
         <div className="md:grid md:grid-cols-2 md:gap-6 lg:grid-cols-3 ">
           <ProjectsCard
             title="FestiSounds"
-            description="FestiSounds is a easy-to-use Web App that allows users find new artists at their favourite festivals. By logging in via Spotify, our powerful algorythms scan your listening history and recommend new artist that we know you'll love at your upcoming music events."
+            description="FestiSounds is an easy-to-use Mobile App that allows users find new artists at their favourite festivals. By logging in via Spotify, our powerful algorithms scan your listening history and recommend new artist that we know you'll love at your upcoming music events."
             previewPath="projects/festisounds/festisounds-preview.png"
             previewAlt="FestiSounds Preview"
             projectURL="https://festisounds.vercel.app/"
           />
           <ProjectsCard
             title="Trim Bounce"
-            description="Trim Bounce is your one-stop-shop for bouncing castle in Co. Meath, Ireland. Whether you're looking for small compact fun, or gruling inflatable challenges, Trim Bounce can cater to every event and party size."
+            description="Trim Bounce is your one-stop-shop for bouncing castles in Co. Meath, Ireland. Whether you're looking for small compact fun, or grueling inflatable challenges, Trim Bounce can cater to every event and party size."
             previewPath="projects/trim-bounce/trim-bounce-preview.png"
             previewAlt="Trim Bounce Preview"
             projectURL="https://www.trimbounce.com/"
           />
           <ProjectsCard
             title="Cocktail Compass"
-            description="Cocktail Compass is an all-in-one bartender's best friend. With the ability to search by name, ingredient and flavour profiles, you'll never be left speachless when the customer doesnt know what they like or want! NB: The full app is currently under re-design & re-construction and may not work as intended."
+            description="Cocktail Compass is an all-in-one bartender's best friend. With the ability to search by name, ingredient and flavour profiles, you'll never be left speechless when the customer doesn't know what they like or want! NB: The full app is currently undergoing a full re-design & re-construction and may not work as fully intended."
             previewPath="projects/cocktail-compass/cocktail-compass-preview.png"
             previewAlt="Cocktail Compass Preview"
             projectURL="https://cocktail-compass-frontend.vercel.app/"
